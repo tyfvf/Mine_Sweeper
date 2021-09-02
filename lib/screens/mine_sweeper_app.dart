76c0bc00d4c4dbf85b54b6d20a8ded889854e854
tyfvf <tyfvf2@gmail.com>
2021-09-01 21:58:43 -0300

    implemented the logic behind mine sweeper, almost complete

--- lib/screens/mine_sweeper_app.dart
+++ lib/screens/mine_sweeper_app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mine_sweeper/components/board_widget.dart';
 import 'package:mine_sweeper/components/result_widget.dart';
 import 'package:mine_sweeper/models/board.dart';
+import 'package:mine_sweeper/models/explosion_exception.dart';
 import 'package:mine_sweeper/models/field.dart';
 
 class MineSweeperApp extends StatefulWidget {
@@ -18,15 +19,41 @@ class _MineSweeperAppState extends State<MineSweeperApp> {
   );
 
   void _restart() {
-    print("restart");
+    setState(() {
+      _win = null;
+      _board.restart();
+    });
   }
 
-  void _open(Field c) {
-    print("open");
+  void _open(Field field) {
+    if (_win != null) {
+        return;
+      }
+    
+    setState(() {
+      try {
+        field.open();
+        if (_board.resolved) {
+          _win = true;
+        }
+      } on ExplosionException {
+        _win = false;
+        _board.revealBombs();
+      }
+    });
   }
 
-  void _alterMark(Field c) {
-    print("Alter Mark");
+  void _alterMark(Field field) {
+    if (_win != null) {
+        return;
+      }
+
+    setState(() {
+      field.alterMark();
+      if (_board.resolved) {
+        _win = true;
+      }
+    });
   }
 
   @override
